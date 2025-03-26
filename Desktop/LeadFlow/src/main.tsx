d7cad20b7c4cbe43f356d4ad084233c168ab28c6
Usuario LeadFlow <usuario@exemplo.com>
2025-03-26 02:17:56 -0300

    Adicionar Toaster do react-hot-toast e ajustar estilos CSS para fonte Figtree

--- Desktop/LeadFlow/src/main.tsx
+++ Desktop/LeadFlow/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 import App from './App.tsx'
 import './index.css'
 
@@ -12,6 +13,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Toaster position="top-right" />
     </BrowserRouter>
   </React.StrictMode>
 );
